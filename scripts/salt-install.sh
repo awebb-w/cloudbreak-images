ec7b8d8e42daf6a864a4f9c0310456a6cc43c31c
awebb-w <56840112+awebb-w@users.noreply.github.com>
2020-12-30 15:15:12 -0500

    Update salt-install.sh
    update salt env variable reference. use os type to hit rhel 7 check to sideload epel

--- scripts/salt-install.sh
+++ scripts/salt-install.sh
@@ -77,8 +77,8 @@ function enable_epel_repository() {
     curl https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -o epel-release-latest-7.noarch.rpm && yum install -y ./epel-release-latest-7.noarch.rpm
   elif [ "${OS}" == "amazonlinux" ] ; then
     yum-config-manager --enable epel
-  elif [ "${OS_TYPE}" == "redhat6" ] ; then
-    curl https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm -o epel-release-latest-6.noarch.rpm && yum install -y ./epel-release-latest-6.noarch.rpm
+  elif [ "${OS_TYPE}" == "redhat7" ] ; then
+    curl https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -o epel-release-latest-7.noarch.rpm && yum install -y ./epel-release-latest-7.noarch.rpm
   else
     yum install -y epel-release
   fi
